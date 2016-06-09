4a88bfc868fe07245502f5bebb7f15e51444baa9
Ben Linville <ben@benlaura.com>
2016-06-09 01:04:57 -0600

    Add table style

--- public/js/app.wioapp.js
+++ public/js/app.wioapp.js
@@ -18,14 +18,36 @@ var wioApp = angular.module('wioApp').config(['$locationProvider', '$routeProvid
     }
 ]);
 
+wioApp.filter("dateFilter", function () {
+    return function (item) {
+        if (item != null) {
+            return new Date(parseInt(item.substr(6)));
+        }
+        return "";
+    };
+});
+
 wioApp.controller('indexController', function indexController($scope) {
     $scope.test = "test value";
 });
 
 wioApp.controller('authListController', function authListController($scope, $http) {
 
+    $scope.username = null;
+    $scope.password = null;
+
     $http.get("http://localhost:3000/svc/auth").then(function(response) {
 
         $scope.accounts = response.data;
     });
+
+    $scope.addAccount = function() {
+
+        $scope.accounts.push({
+           id: 0,
+            username: $scope.username,
+            authtoken: "111",
+            authdate: new Date()
+        });
+    }
 });
